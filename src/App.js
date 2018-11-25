45f8131450f84019b6e1838367b48b57b8a42efa
juworld <juworld@webtoonscorp.com>
2018-11-25 12:54:03 +0900

    add overflowY

--- src/App.js
+++ src/App.js
@@ -2,33 +2,51 @@ import React from 'react';
 import CardList from './CardList'
 import {robots} from './robots'
 import SearchBox from './SearchBox'
-
+import Scroll from './Scroll'
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+        console.log('construct')
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}))
+        console.log('component didmound')
+
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
         // console.log(filteredRobots);
     }
 
     render() {
+        console.log('render')
         const filteredRobots = this.state.robots.filter(
             robots => {
                 return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
             })
+        if (this.state.robots.length === 0) {
+            return <h1> Loading </h1>
+        } else {
         return (
             <div className='tc'>
                 <h1> RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                 <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
+        
         )
+        }
     }
 }
 
